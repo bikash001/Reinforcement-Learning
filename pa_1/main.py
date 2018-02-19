de779d943c1171aac5a3003c2bfd3502ad51e9a5
Bikash Gogoi <bikashgogoi001@gmail.com>
2018-02-19 10:48:23 +0530

    added class for testbed

---
+++ pa_1/main.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+class TestBed(object):
+	"""docstring for TestBed"""
+	def __init__(self, k=10, epsilon=0., init_val=None):
+		super(TestBed, self).__init__()
+		self.arms = k
+		self.arm_means = np.random.normal(size=k)
+		self.values = []
+		self.count = [1] * k
+		self.epsilon = epsilon
+		self.steps = k
+
+		if init_val is None:
+			# pull each arm once
+			for i in range(k):
+				self.values.append(np.random.normal(self.arm_means[i]))
+		else:
+			self.values = [init_val] * k
+
+	def __epsilon_greedy(self):
+		# exploration step
+		if np.random.randn() < self.epsilon:
+			next_arm = np.random.randint(0, self.arms)
+		else:
+			next_arm = np.argmax(self.values)
+
+		reward = np.random.normal(self.arm_means[next_arm])
+		self.values[next_arm] += (reward - self.values[next_arm])/self.count[next_arm]
+		self.count[next_arm] += 1
+
+	def next_action(self):
+		pass
