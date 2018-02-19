8b7a46947c009807b5983cd3210d4a2fe0105d66
Bikash Gogoi <bikashgogoi001@gmail.com>
2018-02-19 15:19:17 +0530

    implemented softmax algorithm

--- pa_1/main.py
+++ pa_1/main.py
@@ -2,7 +2,7 @@ import numpy as np
 
 class TestBed(object):
 	"""docstring for TestBed"""
-	def __init__(self, k=10, epsilon=0., init_val=None):
+	def __init__(self, k=10, epsilon=0., algo='egreedy', init_val=None):
 		super(TestBed, self).__init__()
 		self.arms = k
 		self.arm_means = np.random.normal(size=k)
@@ -24,7 +24,8 @@ class TestBed(object):
 		if np.random.randn() < self.epsilon:
 			next_arm = np.random.randint(0, self.arms)
 		else:
-			next_arm = np.argmax(self.values)
+			min_indices = np.where(self.values == np.max(self.values))[0]
+			next_arm = min_indices[np.random.randint(0, min_indices.shape[0])]
 
 		reward = np.random.normal(self.arm_means[next_arm])
 		self.values[next_arm] += (reward - self.values[next_arm])/self.count[next_arm]
@@ -32,10 +33,25 @@ class TestBed(object):
 		self.steps += 1
 
 	def __softmax(self):
-		pass
+		denom = np.sum(np.exp(self.prefs))
+		for i in range(self.prefs.shape[0]):
+			self.probs[i] = np.exp(self.prefs[i])/denom
+
+		min_indices = np.where(self.prefs == np.max(self.prefs))[0]
+		next_arm = min_indices[np.random.randint(0, min_indices.shape[0])]
+		reward = np.random.normal(self.arm_means[next_arm])
+
+		for i in range(0, next_arm):
+			self.prefs[i] -= (self.alpha * (reward - self.avg_reward)) * self.probs[i]
+
+		self.prefs[next_arm] += self.alpha * (reward - self.avg_reward) * (1 - self.probs[next_reward])
+
+		for i in range(next_arm, self.arms):
+			self.prefs[i] -= (self.alpha * (reward - self.avg_reward)) * self.probs[i]
 
 	def __ucb(self):
-		next_arm = np.argmax(self.values + self.c * np.sqrt(np.log(self.steps)/self.count))
+		min_indices = np.where(self.values == (self.values + self.c * np.sqrt(np.log(self.steps)/self.count)))[0]
+		next_arm = min_indices[np.random.randint(0, min_indices.shape[0])]
 		reward = np.random.normal(self.arm_means[next_arm])
 		self.values[next_arm] += (reward - self.values[next_arm])/self.count[next_arm]
 		self.count[next_arm] += 1
