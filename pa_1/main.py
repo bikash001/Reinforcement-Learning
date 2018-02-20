4256dc09571f07b2c89da994cd6c67eb85cf37a3
Bikash Gogoi <bikashgogoi001@gmail.com>
2018-02-20 10:05:51 +0530

    median elimination started

--- pa_1/main.py
+++ pa_1/main.py
@@ -33,6 +33,7 @@ class TestBed(object):
 		self.steps += 1
 
 	def __softmax(self):
+		self.steps += 1
 		denom = np.sum(np.exp(self.prefs))
 		for i in range(self.prefs.shape[0]):
 			self.probs[i] = np.exp(self.prefs[i])/denom
@@ -40,14 +41,16 @@ class TestBed(object):
 		min_indices = np.where(self.prefs == np.max(self.prefs))[0]
 		next_arm = min_indices[np.random.randint(0, min_indices.shape[0])]
 		reward = np.random.normal(self.arm_means[next_arm])
+		self.avg_reward +=  (reward - self.avg_reward)/self.steps
 
 		for i in range(0, next_arm):
 			self.prefs[i] -= (self.alpha * (reward - self.avg_reward)) * self.probs[i]
 
 		self.prefs[next_arm] += self.alpha * (reward - self.avg_reward) * (1 - self.probs[next_reward])
 
-		for i in range(next_arm, self.arms):
+		for i in range(next_arm+1, self.arms):
 			self.prefs[i] -= (self.alpha * (reward - self.avg_reward)) * self.probs[i]
+
 
 	def __ucb(self):
 		min_indices = np.where(self.values == (self.values + self.c * np.sqrt(np.log(self.steps)/self.count)))[0]
@@ -59,7 +62,17 @@ class TestBed(object):
 
 
 	def __median_elimination(self):
-		pass
+		arms = np.array([i for i in range(0, self.arms)])
+		eps = self.epsilon
+		delta = self.delta
+
+		while arms.shape[0] > 1:
+			times = np.square(2/eps) * np.log(3/delta)
+			means = []
+			for i in arms:
+				means.append(np.mean(np.random.normal(self.means[i],size=times)))
+
+			values = 
 
 	def next_action(self):
 		pass
