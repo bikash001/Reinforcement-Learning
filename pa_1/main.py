f4bd16c3a890503c47a372da7892f2a874a426ae
Bikash Gogoi <bikashgogoi001@gmail.com>
2018-02-20 21:45:07 +0530

    fixed epsilon greedy algorithm

--- pa_1/main.py
+++ pa_1/main.py
@@ -3,21 +3,25 @@ import matplotlib.pyplot as plt
 
 class TestBed(object):
 	"""docstring for TestBed"""
-	def __init__(self, k=10, epsilon=0., algo='egreedy', init_val=None):
+	def __init__(self, k=10, epsilon=0., algo='egreedy', init_val=None, means=None):
 		super(TestBed, self).__init__()
 		self.arms = k
-		self.arm_means = np.random.normal(size=k)
-		self.values = []
-		self.count = np.ones(k)
-		self.epsilon = epsilon
-		self.steps = k
-
-		if init_val is None:
-			# pull each arm once
-			for i in range(k):
-				self.values.append(np.random.normal(self.arm_means[i]))
+		if means is not None:
+			self.arm_means = np.array(means)
 		else:
-			self.values = [init_val] * k
+			self.arm_means = np.random.normal(size=k)
+		self.optimum_arm = np.argmax(self.arm_means)
+		self.values = [0.] * k
+		self.count = np.zeros(k)
+		self.epsilon = epsilon
+		self.steps = 0
+
+		# if init_val is None:
+		# 	# pull each arm once
+		# 	for i in range(k):
+		# 		self.values.append(np.random.normal(self.arm_means[i]))
+		# else:
+		# 	self.values = [init_val] * k
 		self.values = np.array(self.values)
 
 		if algo == 'egreedy':
@@ -31,31 +35,36 @@ class TestBed(object):
 
 	def start_simulation(self):
 		rewards = []
+		arms = []
 		for i in range(1000):
-			reward = self.__algo()
+			reward, arm = self.__algo()
 			rewards.append(reward)
+			arms.append(arm)
 
-		total = rewards[0]
+		total = 0.
+		total_selection = 0.
 		for i in range(1, len(rewards)+1):
 			total += rewards[i-1]
 			rewards[i-1] = total / i
+			total_selection += 1. if arms[i-1] == self.optimum_arm else 0.
+			arms[i-1] = total_selection / i
 
-		return np.array(rewards)
+		return np.array(rewards), np.array(arms)
 	
 
 	def __epsilon_greedy(self):
 		# exploration step
-		if np.random.randn() < self.epsilon:
+		if np.random.rand() < self.epsilon:
 			next_arm = np.random.randint(0, self.arms)
 		else:
-			min_indices = np.where(self.values == np.max(self.values))[0]
-			next_arm = min_indices[np.random.randint(0, min_indices.shape[0])]
+			max_indices = np.where(self.values == np.max(self.values))[0]
+			next_arm = max_indices[np.random.randint(0, max_indices.shape[0])]
 
 		reward = np.random.normal(self.arm_means[next_arm])
-		self.values[next_arm] += (reward - self.values[next_arm])/self.count[next_arm]
 		self.count[next_arm] += 1
+		self.values[next_arm] += (reward - self.values[next_arm])/self.count[next_arm]
 		self.steps += 1
-		return reward
+		return reward, next_arm
 			
 
 	def __softmax(self):
@@ -76,7 +85,7 @@ class TestBed(object):
 
 		for i in range(next_arm+1, self.arms):
 			self.prefs[i] -= (self.alpha * (reward - self.avg_reward)) * self.probs[i]
-		return reward
+		return reward, next_arm
 
 	def __ucb(self):
 		min_indices = np.where(self.values == (self.values + self.c * np.sqrt(np.log(self.steps)/self.count)))[0]
@@ -85,7 +94,7 @@ class TestBed(object):
 		self.values[next_arm] += (reward - self.values[next_arm])/self.count[next_arm]
 		self.count[next_arm] += 1
 		self.steps += 1
-		return reward
+		return reward, next_arm
 
 	def __median_elimination(self):
 		arms = np.array([i for i in range(0, self.arms)])
@@ -101,22 +110,46 @@ class TestBed(object):
 			# values = 
 
 	def next_action(self):
-		pass
+		return self.__algo()
+
+
+def plot():
+	eps = [0., 0.01, 0.1]
+	rewards = [None] * 3
+	arms = [None] * 3
+
+	for i in range(2000):
+		means = np.random.normal(size=10)
+
+		for x, y in enumerate(eps):
+			tb = TestBed(epsilon=y, means=means)
+			if rewards[x] is not None:
+				avg_rewards, avg_arm = tb.start_simulation()
+				rewards[x] += avg_rewards
+				arms[x] += avg_arm
+			else:
+				rewards[x], arms[x] = tb.start_simulation()
+
+	fig = plt.figure()
+	ax1 = fig.add_subplot(211)
+	ax2 = fig.add_subplot(212)
+	for i, x in enumerate(eps):
+		rewards[i] = rewards[i] / 2000
+		arms[i] = arms[i] / 20
+		ax1.plot(list(range(0, len(rewards[i]))), rewards[i], label='eps = '+str(x))
+		ax2.plot(list(range(0, len(arms[i]))), arms[i], label='eps = '+str(x))
+	
+	ax1.legend()
+	ax2.legend()
+	ax1.set_xlabel('Steps')
+	ax1.set_ylabel('Average reward')
+	ax2.set_xlabel('Steps')
+	ax2.set_ylabel('% Optimal action')
+	ax2.set_ylim(0, 100)
+	ax1.set_ylim(0)
+	plt.show()
+
 
 
 if __name__ == '__main__':
-	for eps in [0., 0.1, 0.01]:
-		rewards = None
-		for i in range(2000):
-			tb = TestBed(epsilon=eps)
-			if rewards is not None:
-				avg_rewards = tb.start_simulation()
-				rewards += avg_rewards
-			else:
-				rewards = tb.start_simulation()
-
-		rewards = rewards / 2000
-		plt.plot(list(range(0, len(rewards))), rewards, label='eps = '+str(eps))
-	
-	plt.legend()
-	plt.show()
+	plot()
